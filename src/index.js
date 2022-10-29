cddc587c675b765c17f70d64d242179054a2be24
Yaroslav <yaroslavkopylov98@gmail.com>
2022-10-29 21:39:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,104 +1,89 @@
-import "./sass/index.scss";
+import './sass/index.scss';
 
-import NewsApiService from "./js/api-service";
-import renderCard from "./js/renderCard";
-import { lightbox } from "./js/onslidermake";
+import NewsApiService from './js/api-service';
+import renderCard from './js/renderCard';
+import { lightbox } from './js/onslidermake';
 //import axios from 'axios';
-import { Notify } from "notiflix/build/notiflix-notify-aio";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const searchForm = document.querySelector(".search-form"),
-    galleryContainer = document.querySelector(".gallery"),
-    loadMoreBtn = document.querySelector(".load-more"),
-    searchBtn = document.querySelector(".search");
+const searchForm = document.querySelector('.search-form'),
+  galleryContainer = document.querySelector('.gallery'),
+  loadMoreBtn = document.querySelector('.load-more'),
+  searchBtn = document.querySelector('.search');
 
 let isShown = 0;
 const newsApiService = new NewsApiService();
 
-searchForm.addEventListener("submit", onSearch);
-loadMoreBtn.addEventListener("click", onLoadMore);
-searchBtn.addEventListener("click", () => {
-    Notify.success(`Hooray! We found 500 images !!!`);
-});
-
-// Бесконечного скролла
-// const options = {
-//     rootMargin: "50px",
-//     root: null,
-//     threshold: 0.3,
-// };
-// const observer = new IntersectionObserver(onLoadMore, options);
-//observer.observe(refs.loadMoreBtn);
+searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
 //////---- FUNCTION ----////
 function onSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    galleryContainer.innerHTML = "";
-    newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-    newsApiService.resetPage();
+  galleryContainer.innerHTML = '';
+  newsApiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  newsApiService.resetPage();
 
-    if (newsApiService.query === "") {
-        Notify.warning("Please, fill the main field");
-        return;
-    }
+  if (newsApiService.query === '') {
+    Notify.warning('Please, fill the main field');
+    return;
+  }
 
-    isShown = 0;
-    fetchGallery();
-    onRenderGallery(hits);
-
-    ///// если нужен бесконечній скролл
-    //observer.observe(refs.loadMoreBtn);
+  isShown = 0;
+  fetchGallery();
+  onRenderGallery(hits);
 }
 
 function onLoadMore() {
-    newsApiService.page;
-    fetchGallery();
+  newsApiService.page;
+  fetchGallery();
 }
-
+searchBtn.addEventListener('click', () => {
+  Notify.success(`Hooray! We found 500 images !!!`);
+});
 async function fetchGallery() {
-    loadMoreBtn.classList.add("is-hidden");
+  loadMoreBtn.classList.add('is-hidden');
 
-    const r = await newsApiService.fetchGallery();
-    const { hits, totalHits } = r;
-    isShown += hits.length;
+  const r = await newsApiService.fetchGallery();
+  const { hits, totalHits } = r;
+  isShown += hits.length;
 
-    if (!hits.length) {
-        Notify.warning(
-            `Sorry, there are no images matching your search query. Please try again.`
-        );
-        loadMoreBtn.classList.add("is-hidden");
-        return;
-    }
+  if (!hits.length) {
+    Notify.warning(
+      `Sorry, there are no images matching your search query. Please try again.`
+    );
+    loadMoreBtn.classList.add('is-hidden');
+    return;
+  }
 
-    onRenderGallery(hits);
-    isShown += hits.length;
+  onRenderGallery(hits);
+  isShown += hits.length;
 
-    if (isShown < totalHits) {
-        // Показывает кнопку
-        // Notify.success(`Hooray! We found ${totalHits} images !!!`);
-        loadMoreBtn.classList.remove("is-hidden");
-    }
-    // Если пользователь дошел до конца коллекции, пряч кнопку и выводи уведомление с текстом:
-    if (isShown >= totalHits) {
-        Notify.info(
-            "We re sorry, but you have reached the end of search results."
-        );
-    }
+  if (isShown < totalHits) {
+    // Показывает кнопку
+    // Notify.success(`Hooray! We found ${totalHits} images !!!`);
+    loadMoreBtn.classList.remove('is-hidden');
+  }
+  // Если пользователь дошел до конца коллекции, пряч кнопку и выводи уведомление с текстом:
+  if (isShown >= totalHits) {
+    Notify.info('We re sorry, but you have reached the end of search results.');
+  }
 }
 
 function onRenderGallery(elements) {
-    const markup = elements
-        .map(
-            ({
-                webformatURL,
-                largeImageURL,
-                tags,
-                likes,
-                views,
-                comments,
-                downloads,
-            }) => {
-                return `<div class="photo-card">
+  const markup = elements
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
     <a href="${largeImageURL}">
       <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
     </a>
@@ -121,9 +106,9 @@ function onRenderGallery(elements) {
       </p>
     </div>
     </div>`;
-            }
-        )
-        .join("");
-    galleryContainer.insertAdjacentHTML("beforeend", markup);
-    lightbox.refresh();
+      }
+    )
+    .join('');
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
